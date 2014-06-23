b844b57997c90cfd19324fe4b4d76bb9152f8f1c
David Li <li.davidm96@gmail.com>
2014-06-23 09:50:35 -0700

    Don't show TOC on non-post pages

--- src/js/headers.js
+++ src/js/headers.js
@@ -45,31 +45,4 @@ $(function() {
             }
         }
     }
-
-
-    // Mobile menu
-    var visible = false;
-    $('#mobile-menu').click(function() {
-        if (visible) {
-            $('header #navigation').css({
-                opacity: 0,
-                '-webkit-transform': 'translateX(-100%)',
-                '-moz-transform': 'translateX(-100%)',
-                '-o-transform': 'translateX(-100%)',
-                '-ms-transform': 'translateX(-100%)',
-                'transform': 'translateX(-100%)'
-            });
-        }
-        else {
-            $('header #navigation').css({
-                opacity: 1,
-                '-webkit-transform': 'translateX(0%)',
-                '-moz-transform': 'translateX(0%)',
-                '-o-transform': 'translateX(0%)',
-                '-ms-transform': 'translateX(0%)',
-                'transform': 'translateX(0%)'
-            });
-        }
-        visible = !visible;
-    });
 });
